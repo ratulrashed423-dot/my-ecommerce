4ec53802807dfe30f7c0b1de6f894c67004fd368
ratulrashed423-dot <ratulrashed423@gmail.com>
2026-03-11 13:42:32 +0600

    Add Home component for e-commerce website

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to My E-commerce Website</h1>
+      <p>This is a starter page. 🛒</p>
+    </div>
+  );
+}
